da10074dc04577017b90fef41ef622f862ef6a94
Jordi Prats <jprats@systemadmin.es>
2016-07-11 10:13:18 +0200

    no te servei

--- manifests/service.pp
+++ manifests/service.pp
@@ -7,15 +7,15 @@ class jpackage::service inherits jpackage {
 
   validate_re($jpackage::service_ensure, [ '^running$', '^stopped$' ], "Not a valid daemon status: ${jpackage::service_ensure}")
 
-  $is_docker_container_var=getvar('::eyp_docker_iscontainer')
-  $is_docker_container=str2bool($is_docker_container_var)
-
-  if( $is_docker_container==false or
-      $jpackage::manage_docker_service)
-  {
-    if($jpackage::manage_service)
-    {
-      #service or exec here
-    }
-  }
+  # $is_docker_container_var=getvar('::eyp_docker_iscontainer')
+  # $is_docker_container=str2bool($is_docker_container_var)
+  #
+  # if( $is_docker_container==false or
+  #     $jpackage::manage_docker_service)
+  # {
+  #   if($jpackage::manage_service)
+  #   {
+  #     #service or exec here
+  #   }
+  # }
 }
